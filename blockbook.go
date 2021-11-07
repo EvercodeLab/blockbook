06e64809ac1fa3456b434a1aee66bf0ef2e2b982
kalexey89 <kyznecov.alexey@gmail.com>
2021-11-08 01:08:58 +0300

    fix(bsc): remove commented code
    (cherry picked from commit 2a601c0b6b516efaccfadb03509c82fe8c33c55e)

--- blockbook.go
+++ blockbook.go
@@ -208,10 +208,6 @@ func mainWithExitCode() int {
 	}
 
 	if internalState.DbState != common.DbStateClosed {
-		//if internalState.DbState == common.DbStateInconsistent {
-		//	glog.Error("internalState: database is in inconsistent state and cannot be used")
-		//	return exitCodeFatal
-		//}
 		glog.Warning("internalState: database was left in open state, possibly previous ungraceful shutdown")
 	}
 
